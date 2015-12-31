5c7e69a2ba4f149cfdbaa1181513a442fea9d2e8
numanobue@gmail.com <manu@localhost.(none)>
2015-12-31 16:56:23 +0100

    compile, a tester

--- Info_embarquee/Raspberry/Code_principal/pince.c
+++ Info_embarquee/Raspberry/Code_principal/pince.c
@@ -44,39 +44,37 @@ int main(void)
 	if (wiringPiSetup()<0){printf("Unable to setup wiringPi\n");exit(1);}
 	else{printf("Wiring setup\n");}
 
-	//---------------- Demande le nom du fichier dans lequel écrire et le crée
-	char path[20];
+	//---------------- Demande le nom du fichier dans lequel écrire les angles et le crée
+	char filename[20],path[20];
 	printf("Entrez le nom du fichier: ");
-	scanf("%s",path);
-	FILE* fd = fopen(path,"w");
+	scanf("%s",filename);
+	FILE* fd = fopen(filename,"w");
 	if (fd<0){printf("unable to open/create file\n");exit(-1);}
 
 
 	//---------------- Connexion à l'arduino
-	int connected=0,i=0,se,se_temp;
+	int connected=0,i=0,se=-1,se_temp;
 	while(connected==0 && i<5)
 	{
         //Chemin du port Série i
 		sprintf(path,"/dev/ttyACM%d",i);
-		se_temp = serialOpen(path,baudrate);
+		se_temp = serialOpen(path,BAUDRATE);
         //Si ce Port est connecté
 		if (se_temp>0)
 		{
             //L'arduino Reset automatiquement, on attend qu'elle soit opérationelle
 			delay(2000);
             // On demandee l'ID de l'arduino connectée
-			ID = Ardu_ID(se_temp);
-			if (ID == '2')
+			if (Ardu_ID(se_temp) == '2')
 			{
                 //on attribue le se récupéré à l'ouverture du port au fd qui servira à la communiquer avec l'arduino
 				se = se_temp;
-				N_ardus++;
 				printf("	Arduino 2 connected !\tse: %d\n",se);
 			}
 		}
 		i++;
 	}
-
+	if (se<0){printf("Unable to detect arduino, check if /dev/ttyACMx exists and the appropriate program is running on arduino\n");exit(-1);}
 	delay(5);
 	//On demande les angles à l'arduino
 	serialPutstringln(se,"getangles",9);
@@ -85,9 +83,9 @@ int main(void)
 	//On lit les réponses et on les stocke dans le fichier
 	for(i=0;i<8;i++)
 	{
-		ID = serialGetchar(se)+48;
-		position=serialGetfloat(se);
-		fprintf(fd,"%c %d\n",ID,position);
+		int ID = serialGetchar(se);
+		int position=serialGetfloat(se);
+		fprintf(fd,"%d %d\n",ID,position);
 	}
 	
 	serialClose(se);
